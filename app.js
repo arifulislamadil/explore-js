1434ccb3bd2236e90cbf35a10f8ea0f2d9b2a09d
Ariful Islam Adil <adil.islam.arif@gmail.com>
2023-01-26 00:14:28 +0400

    added few lines

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
-const names = [2,4,5,9,3];
-names.push(8);
-console.log(names);
+const numbers = [2,4,5,9,3];
+const names ={
+    name:"arif", 
+    age: 22,
+    pro: "web developer"
+}
+
+
+for(const name in names){
+    const value = names[name]
+ console.log(value);
+}
+
+
+let x = 3;
+x++;
+console.log(x);
+
+const price1= 2;
+const price2 = "2";
+if(price1==price2){
+    console.log("same");
+}
+const num = "2";
+const numToSt = parseInt(num)
+console.log(typeof(numToSt));
+
+
+const gpa = "2.4";
+const getNumber = parseFloat(gpa);
+console.log(getNumber);
+console.log(typeof(getNumber));
+
+const modu = 3;
+const toModu = modu % 2;
+console.log(toModu);
